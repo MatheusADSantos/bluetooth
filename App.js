d57c87eba6b31008a8d5392f203127a1cc062bc7
MatheusADSantos <matheusdevmobile2020@gmail.com>
2021-03-29 11:40:03 -0300

    Modify ...

--- App.js
+++ App.js
@@ -39,6 +39,7 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
+
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
@@ -58,13 +59,9 @@ const App: () => React$Node = () => {
                 <DebugInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
+            
             <LearnMoreLinks />
+
           </View>
         </ScrollView>
       </SafeAreaView>
